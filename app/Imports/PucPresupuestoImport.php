5c83c13768fde25f2d99f10935666e0d158ad721
aruisan <oskrjag@gmail.com>
2022-08-11 16:27:55 +0000

    server change

--- app/Imports/PucPresupuestoImport.php
+++ app/Imports/PucPresupuestoImport.php
@@ -13,15 +13,11 @@ class PucPresupuestoImport implements ToModel
     * @return \Illuminate\Database\Eloquent\Model|null
     */
 
-<<<<<<< HEAD
-    public function model(array $row, $vigencia_id)
-=======
     public function  __construct(string $vigencia) {
         $this->vigencia = $vigencia;
     }
 
     public function model(array $row)
->>>>>>> efe1ffda340d5683f7b878834440b82a8ecbd99a
     {
         // \Log::debug($row);
         if(!is_null($row[0])):
